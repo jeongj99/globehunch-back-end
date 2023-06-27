a89fb36108ee4775413cd5149405e2e4d2d17c32
Jae Yun Jeong <jeongalex0616@gmail.com>
2023-06-27 14:32:40 -0400

    Create server controller for express session

---
+++ server/serverController.js
@@ -0,0 +1,15 @@
+const session = require('express-session');
+const PGSession = require('connect-pg-simple')(session);
+const client = require('./db/index');
+
+const sessionMiddleware = session({
+  store: new PGSession({ client }),
+  secret: process.env.SESSION_KEY,
+  resave: false,
+  cookie: {
+    httpOnly: true,
+    maxAge: parseInt(process.env.SESSION_MAX_AGE, 10)
+  }
+});
+
+module.exports = { sessionMiddleware };
