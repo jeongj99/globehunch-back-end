0a577aa0903affeb945f34eded692d578014e284
Jae Yun Jeong <jeongalex0616@gmail.com>
2023-06-27 15:29:14 -0400

    Create JWT file

---
+++ server/JWT.js
@@ -0,0 +1,35 @@
+const { sign, verify } = require('jsonwebtoken');
+
+const createToken = user => {
+  const accessToken = sign(
+    {
+      id: user.id,
+      username: user.user_name
+    },
+    process.env.JWT_SECRET
+  );
+
+  return accessToken;
+};
+
+const validateToken = (req, res, next) => {
+  const { accessToken } = req.session;
+
+  if (!accessToken) {
+    return res.status(400).json({ error: 'User not authenticated' });
+  }
+
+  verify(accessToken, process.env.JWT_SECRET, (error, decoded) => {
+    if (error) {
+      return res.status(400).json({ error });
+    }
+    req.authenticated = true;
+    req.user = {
+      id: decoded.id,
+      username: decoded.username
+    };
+    return next();
+  });
+};
+
+module.exports = { createToken, validateToken };
