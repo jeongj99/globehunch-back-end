2dfde69046082348b9d782fcd43cf0281bbcbd6b
Jae Yun Jeong <jeongalex0616@gmail.com>
2023-08-29 16:54:49 -0400

    Update

--- server/db/index.js
+++ server/db/index.js
@@ -1,13 +1,8 @@
 const pg = require("pg");
 
 const client = new pg.Client({
-  host: process.env.PGHOST,
-  user: process.env.PGUSER,
-  database: process.env.PGDATABASE,
-  password: process.env.PGPASSWORD,
-  port: process.env.PGPORT,
-  ssl: process.env.PGHOST !== 'localhost', // Enable SSL unless the host is 'localhost'
-  rejectUnauthorized: false // Bypass SSL certificate validation for testing (only use for local development)
+  connectionString: process.env.DATABASE_URL || "",
+  ssl: process.env.DATABASE_URL ? { rejectUnauthorized: false } : false
 });
 
 client
